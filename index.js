78e84e9b228ff5c2dc34a15fcf798b60d685be18
asjorok <asjorok@gmail.com>
2020-02-02 18:37:34 +0100

    Added launch config

--- index.js
+++ index.js
@@ -2,20 +2,43 @@ const { Writer, Reader } = require('nsqjs');
 
 const channel = 'planet.list';
 const r = new Reader(channel, 'handle', {
-    lookupdHTTPAddresses: 'localhost:4161'
+    lookupdHTTPAddresses: '127.0.0.1:4161'
 });
 const w = new Writer('127.0.0.1', 4150);
 
 
 // Register handler for data
 r.on('message', (message) => {
-    console.log('GOT MQ MESSAGE');
-    console.log(message);
 
+    try {
+        const data = JSON.parse(message.body.toString());
+        console.log("MESSAGE REQUEST");
+        console.log(data.request);
+        console.log("MESSAGE RESPONSE TOPIC");
+        console.log(data.responseChannel);
+
+        respond(data.responseChannel);
+
+    }
+    catch (error) {
+        console.log("Message handling failed!");
+        consolelog(error);
+    }
+    finally {
+        message.finish();
+    }
     // Request data
     //w.publish("planet.list.TESTRETURN", "fin", console.log);
     message.finish();
 });
+
+function respond(topic) {
+    w.publish(topic, JSON.stringify({ planets: [1, 5, 17] }), (error) => {
+        if (!error) return;
+        console.log("MQ ERROR");
+        console.error(error);
+    });
+}
 
 r.connect();
 w.connect();
