1bea42cc4f2487d1f2e02a7962cfb32062d88836
asjorok <asjorok@gmail.com>
2020-02-28 17:04:00 +0100

    Use db to list planets

--- planet.list.js
+++ planet.list.js
@@ -1,17 +1,39 @@
 const { Worker } = require('redis-request-broker');
-const queue = 'planet.list';
+const { Client } = require('pg');
 
+const queue = 'planet.list';
+const client = new Client();
+const worker = new Worker(queue, handle);
 
 async function handle(data) {
-    console.log(`Responding to request '${data}'`);
+    console.log(`Got request:`, data);
+    const planets = await client.query("select * from planets");
     return {
-        planets: [
-            { id: 1, name: 'Planet 1' }, { id: 5, name: 'Planet 5' }, { id: 18, name: 'Planet 18' }
-        ]
+        planets: planets.rows
     };
 }
 
-const w = new Worker(queue, handle);
-w.listen();
+async function start() {
+    try {
+        await client.connect();
+        await worker.listen();
+        console.log("Listening...");
+    }
+    catch (error) {
+        console.error("Failed to connect", error);
+    }
+}
 
-process.on('beforeExit', async () => w.stop());
+
+process.on('beforeExit', async () => {
+    try {
+        await worker.stop();
+        await client.end();
+        console.log("Disconnected.");
+    }
+    catch (error) {
+        console.error("Failed to disconnect", error);
+    }
+});
+
+start();
